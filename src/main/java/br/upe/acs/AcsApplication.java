f4e941182e8dcb12bf3b38d02c2e831e221ac0ae
José Ferreira <josebazjunior09@gmail.com>
2023-05-31 13:09:43 -0300

    adicionando estrutura do projeto

---
+++ src/main/java/br/upe/acs/AcsApplication.java
@@ -0,0 +1,13 @@
+package br.upe.acs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/upe/acs/AcsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.acs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
