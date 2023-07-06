ce4dbe36d27e3a5ff00fde99c997766c8a9fb1bf
Ana Laura <analaurass45@gmail.com>
2023-07-06 14:48:55 -0300

    feat: configuracao do flyway

---
+++ src/main/resources/db/migration/V3__insertCurso.sql
@@ -0,0 +1,104 @@
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (1, 180, 'Ciências Biológicas');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (2, 180, 'Ciências Sociais');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (3, 180, 'Computação');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (4, 180, 'Educação Física');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (5, 180, 'Geografia');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (6, 180, 'História');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (7, 180, 'Letras com habilitação em língua portuguesa e espanhola');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (8, 180, 'Letras com habilitação em língua portuguesa e inglesa');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (9, 180, 'Letras com habilitação em língua portuguesa e suas literaturas');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (10, 180, 'Matemática');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (11, 180, 'Pedagogia');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (12, 180, 'Administração');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (13, 180, 'Ciências biológicas');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (14, 180, 'Direito');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (15, 180, 'Educação Física');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (16, 180, 'Enfermagem');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (17, 180, 'Engenharia civil');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (18, 180, 'Engenharia da computação');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (19, 180, 'Engenharia de controle e automação');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (20, 180, 'Engenharia de software');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (21, 180, 'Engenharia elétrica de telecomunicações');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (22, 180, 'Engenharia elétrica eletrônica');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (23, 180, 'Engenharia elétrica eletrotécnica');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (24, 180, 'Engenharia mecânica');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (25, 180, 'Física de materiais');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (26, 180, 'Fisioterapia');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (27, 180, 'Medicina');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (28, 180, 'Nutrição');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (29, 180, 'Odontologia');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (30, 180, 'Psicologia');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (31, 180, 'Saúde coletiva');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (32, 180, 'Serviço social');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (33, 180, 'Sistemas de informação');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (34, 180, 'Terapia ocupacional');
+
+INSERT INTO public.curso(id, horas_complementares, nome)
+	VALUES (35, 180, 'Tecnologia em logística');
