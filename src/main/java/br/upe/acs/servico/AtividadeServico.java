ee662db0193cf3fe78967f95c513e79c5117316a
rikcav <henriquearau000@gmail.com>
2023-08-21 17:09:32 -0300

    feat: adicionado método de exclusão de atividade

--- src/main/java/br/upe/acs/servico/AtividadeServico.java
+++ src/main/java/br/upe/acs/servico/AtividadeServico.java
@@ -1,8 +1,10 @@
 package br.upe.acs.servico;
 
 import br.upe.acs.dominio.Atividade;
+import br.upe.acs.dominio.Certificado;
 import br.upe.acs.dominio.Endereco;
 import br.upe.acs.dominio.dto.AtividadeDTO;
+import br.upe.acs.dominio.enums.CertificadoStatusEnum;
 import br.upe.acs.dominio.enums.EixoEnum;
 import br.upe.acs.repositorio.AtividadeRepositorio;
 import br.upe.acs.utils.AcsExcecao;
@@ -17,44 +19,43 @@ import java.util.Optional;
 public class AtividadeServico {
 
 	private final AtividadeRepositorio repositorio;
-	
+
 	public List<Atividade> listarAtividades() {
 		return repositorio.findAll();
 	}
-	
+
 	public Atividade buscarAtividadePorId(Long id) throws AcsExcecao {
 		Optional<Atividade> atividade = repositorio.findById(id);
 		if (atividade.isEmpty()) {
 			throw new AcsExcecao("Não existe uma atividade associada a este id!");
 		}
-		
+
 		return atividade.get();
 	}
-	
+
 	public List<Atividade> buscarAtividadePorEixo(String eixo) throws AcsExcecao{
-		boolean existe = false;		
+		boolean existe = false;
 		EixoEnum eixoFormato = null;
 		for(EixoEnum c : EixoEnum.values()) {
 			if(c.toString().equalsIgnoreCase(eixo)) {
 				existe = true;
 				eixoFormato = c;
-			};			
+			};
 		}
 		if(!existe) {
 			throw new AcsExcecao("Não existe um eixo correspondente");
 		}
-		
+
 		List<Atividade> atividade = repositorio.findByEixo(eixoFormato);
 
 		if(atividade.isEmpty()) {
 			throw new AcsExcecao("Não existe atividade associada a esse eixo");
 		}
-		
+
 		return atividade;
 	}
 
 	public Atividade criarAtividade(AtividadeDTO atividade) throws AcsExcecao {
-
 		Atividade atividadeNova = new Atividade();
 		atividadeNova.setEixo(atividade.getEixo());
 		atividadeNova.setDescricao(atividade.getDescricao());
@@ -62,4 +63,9 @@ public class AtividadeServico {
 		atividadeNova.setChPorCertificado(atividade.getChPorCertificado());
 		return atividadeNova;
 	}
+
+	public void excluirAtividade(Long id) throws AcsExcecao {
+		Atividade atividade = buscarAtividadePorId(id);
+        repositorio.deleteById(id);
+    }
 }
