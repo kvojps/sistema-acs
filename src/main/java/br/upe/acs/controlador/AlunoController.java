bb625392c25c8699016b46c0974551acd9f27154
Jose Ferreira <josebazjunior09@gmail.com>
2023-09-24 02:52:30 -0300

    [fix]: refactor aluno controller

---
+++ src/main/java/br/upe/acs/controlador/AlunoController.java
@@ -0,0 +1,33 @@
+package br.upe.acs.controlador;
+
+import br.upe.acs.config.JwtService;
+import br.upe.acs.dominio.vo.AtividadeComplementarVO;
+import br.upe.acs.dominio.vo.MinhasHorasNaAtividadeVO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import br.upe.acs.servico.StudentService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/student/utilization")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class AlunoController {
+
+    private final StudentService service;
+    private final JwtService jwtService;
+
+    @GetMapping
+    public ResponseEntity<AtividadeComplementarVO> getStudentAcs(HttpServletRequest request) {
+        String email = jwtService.extractUsername(request.getHeader("Authorization").substring(7));
+        return ResponseEntity.ok(service.getStudentAcs(email));
+    }
+
+    @GetMapping("/horas/{activityId}")
+    public ResponseEntity<MinhasHorasNaAtividadeVO> getActivityHours(HttpServletRequest request, @PathVariable("activityId") Long activityId) {
+        String email = jwtService.extractUsername(request.getHeader("Authorization").substring(7));
+        return ResponseEntity.ok(service.getHoursAcsStatusByActivity(email, activityId));
+    }
+}
