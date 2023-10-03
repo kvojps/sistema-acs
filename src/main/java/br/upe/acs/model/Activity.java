e9ac8b146e8b531b46cbb90edc84ac240797f535
amadeusantos <amadeusantos31@gmail.com>
2023-10-03 00:01:36 -0300

    [fix] change generation id and scripts sql

--- src/main/java/br/upe/acs/model/Activity.java
+++ src/main/java/br/upe/acs/model/Activity.java
@@ -21,7 +21,7 @@ import lombok.NoArgsConstructor;
 public class Activity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@Enumerated(EnumType.STRING)
--- src/main/java/br/upe/acs/model/User.java
+++ src/main/java/br/upe/acs/model/User.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class User implements UserDetails {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	private String fullName;
